85d35031429c08b2d8e989bccc51455c47b7fefa
mmble <23295125+mmble@users.noreply.github.com>
2019-11-24 03:42:22 +0100

    Detecting if entry exist, more informations are printed.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # using external library - requests | to install use >pip install requests
 import os
 import re
+import sys
 import json
 import requests
 
@@ -17,32 +18,78 @@ def printdata(type, data):
     if len(data) > 1:
       print("Authors: " + datastring)
     elif len(data) == 1:
-      print("Authors: " + datastring)
+      print("Author: " + datastring)
     else:
       print("No informations about author/s are available :/")
   elif type == "title":
-    print("Title: "+data[0]+" - "+data[1])
-  elif type == "publishing":
-    print("Published by "+data[0]+" in "+data[1])
-
-
-while True:
-  getinput = input()
-  if re.search(r'^\d{13}$', getinput):
-    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + getinput)
-    if response.status_code != 200:
-      print("Error: "+response.status_code)
+    if len(data) == 2:
+      print("Title: "+data[0]+" - "+data[1])
     else:
-      getjson = response.json()
-      if getjson["totalItems"] != 1:
-        if getjson["totalItems"] == 0:
-          print("This item do not exist in Google Books database :(")
-        else:
-          print("Error: there is more than one book.")
+      print("Title: "+data[0])
+  elif type == "publishing":
+    print("Published by "+data[0]+" in "+data[1]+"yr")
+  elif type == "others":
+    print("Pages: "+str(data[0])+"; Language: "+data[1]+"; Age-rating: "+data[2]+"; Public domain: "+str(data[3]))
+
+try:
+  while True:
+    # TODO - save the api response to file for some time, if book was checked before read data from that file
+    getinput = input('Barcode scanner\n$ ')
+    if re.search(r'^\d{13}$', getinput):
+      response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + getinput)
+      if response.status_code != 200:
+        print("Error: "+response.status_code)
       else:
-        booksinfoitems = getjson["items"][0]
-        printdata("title", [booksinfoitems["volumeInfo"]["title"], booksinfoitems["volumeInfo"]["subtitle"]])
-        printdata("authors", booksinfoitems["volumeInfo"]["authors"])
-        printdata("publishing", [booksinfoitems["volumeInfo"]["title"], booksinfoitems["volumeInfo"]["subtitle"]])
-  else:
-    os.system(getinput)
+        getjson = response.json()
+        if getjson["totalItems"] != 1:
+          if getjson["totalItems"] == 0:
+            print("This item do not exist in Google Books database :(")
+          else:
+            print("Error: there is more than one book.")
+        else:
+          booksinfoitems = getjson["items"][0]
+
+          if "subtitle" in booksinfoitems["volumeInfo"]:
+            printdata("title", [booksinfoitems["volumeInfo"]["title"], booksinfoitems["volumeInfo"]["subtitle"]])
+          else:
+            printdata("title", [booksinfoitems["volumeInfo"]["title"]])
+
+          if "authors" in booksinfoitems["volumeInfo"]:
+            printdata("authors", booksinfoitems["volumeInfo"]["authors"])
+          else:
+            printdata("authors", [])
+
+          if "publisher" not in booksinfoitems["volumeInfo"]:
+            booksinfoitems["volumeInfo"]["publisher"] = "-"
+          if "publishedDate" not in booksinfoitems["volumeInfo"]:
+            booksinfoitems["volumeInfo"]["publisher"] = "-"
+          printdata("publishing", [booksinfoitems["volumeInfo"]["publisher"], booksinfoitems["volumeInfo"]["publishedDate"]])
+          if "pageCount" not in booksinfoitems["volumeInfo"]:
+            booksinfoitems["volumeInfo"]["pageCount"] = "-"
+          if "language" not in booksinfoitems["volumeInfo"]:
+            booksinfoitems["volumeInfo"]["language"] = "-"
+          if "maturityRating" not in booksinfoitems["volumeInfo"]:
+            booksinfoitems["volumeInfo"]["maturityRating"] = "-"
+          else:
+            if booksinfoitems["volumeInfo"]["maturityRating"] == "NOT_MATURE":
+              booksinfoitems["volumeInfo"]["maturityRating"] = "not mature"
+            else:
+              booksinfoitems["volumeInfo"]["maturityRating"] = booksinfoitems["volumeInfo"]["maturityRating"].lower()
+          if "publicDomain" not in booksinfoitems["accessInfo"]:
+            booksinfoitems["accessInfo"]["publicDomain"] = "-"
+          printdata("others", [booksinfoitems["volumeInfo"]["pageCount"], booksinfoitems["volumeInfo"]["language"], booksinfoitems["volumeInfo"]["maturityRating"], booksinfoitems["accessInfo"]["publicDomain"]])
+          print()
+    elif getinput == 'exit':
+      try:
+        sys.exit(0)
+      except SystemExit:
+        os._exit(0)
+    else:
+      os.system(getinput)
+
+except KeyboardInterrupt:
+  print('\nInterrupted')
+  try:
+    sys.exit(0)
+  except SystemExit:
+    os._exit(0)
